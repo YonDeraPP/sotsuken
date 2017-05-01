8f238d16b2af6af5e4da75e76a8215e951817439
dera <deraderapyon@gmail.com>
2017-05-01 15:15:23 +0900

    sn

--- facedetect.py
+++ facedetect.py
@@ -1,124 +1,56 @@
-#!/usr/bin/python
-"""
-This program is demonstration for face and object detection using haar-like features.
-The program finds faces in a camera image or video stream and displays a red box around them.
+import io
+import picamera
+import cv2
 
-Original C implementation by:  ?
-Python implementation by: Roman Stanchak, James Bowman
-"""
-import sys
-import cv2.cv as cv
-from optparse import OptionParser
+import numpy as np
 
-# Parameters for haar detection
-# From the API:
-# The default parameters (scale_factor=2, min_neighbors=3, flags=0) are tuned
-# for accurate yet slow object detection. For a faster operation on real video
-# images the settings are:
-# scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING,
-# min_size=<minimum possible face size
+# 正面の顔の情報データ
+cascade_path = "/ usr / share / opencv / haarcascades / haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_path)
 
-min_size = (20, 20)
-image_scale = 2
-haar_scale = 1.2
-min_neighbors = 2
-haar_flags = 0
+stream = io.BytesIO()
 
+# カメラの解像度を設定
+CAMERA_WIDTH = 320
+CAMERA_HEIGHT = 240
 
-def detect_and_draw(img, cascade):
-    # allocate temporary images
-    gray = cv.CreateImage((img.width, img.height), 8, 1)
-    small_img = cv.CreateImage((cv.Round(img.width / image_scale),
-                                cv.Round(img.height / image_scale)), 8, 1)
+#
+color = (255, 255, 255)
+camera = picamera.PiCamera()
+camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
 
-    # convert color input image to grayscale
-    cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
+# カメラで何回写真を取るかのループ
+for i in xrange(5):
+    # 写真を撮る！
+    camera.capture(stream, format='jpeg')
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    image = cv2.imdecode(data, 1)
 
-    # scale input image for faster processing
-    cv.Resize(gray, small_img, cv.CV_INTER_LINEAR)
+    # エラーになるのでコメントアウト(ウィンドウに表示)
+    #        cv2.imshow('image',image)
+    #        cv2.waitKey(16)
 
-    cv.EqualizeHist(small_img, small_img)
+    # 解析しやすくするために灰色にする
+    image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+    # ライブラリに顔検出してもらう
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
-    if (cascade):
-        t = cv.GetTickCount()
-        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
-                                     haar_scale, min_neighbors, haar_flags, min_size)
-        t = cv.GetTickCount() - t
-        print
-        "detection time = %gms" % (t / (cv.GetTickFrequency() * 1000.))
-        if faces:
-            for ((x, y, w, h), n) in faces:
-                # the input to cv.HaarDetectObjects was resized, so scale the
-                # bounding box of each face and convert it to two CvPoints
-                pt1 = (int(x * image_scale), int(y * image_scale))
-                pt2 = (int((x + w) * image_scale), int((y + h) * image_scale))
-                cv.Rectangle(img, pt1, pt2, cv.RGB(255, 0, 0), 3, 8, 0)
+    image_output = image
+    # 顔が１つ以上あったら
+    if len(facerect) > 0:
+        # 顔検出した部分を、四角で囲む
+        for rect in facerect:
+            cv2.rectangle(image_output, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+        #
+        print "found"
+    # エラーになるのでコメントアウト(ウィンドウに表示)
+    #        cv2.imshow('image_out',image_output)
+    #        cv2.waitKey(16)
 
-    cv.ShowImage("result", img)
+    # jpgファイルに保存
+    cv2.imwrite(str(i) + '.jpg', image)
 
+    # 写真領域をクリア(コレをしないと、ずっと同じ写真になる)
+    stream.seek(0)
 
-if __name__ == '__main__':
-
-    parser = OptionParser(usage="usage: %prog [options] [filename|camera_index]")
-    parser.add_option("-c", "--cascade", action="store", dest="cascade", type="str",
-                      help="Haar cascade file, default %default",
-                      default="../data/haarcascades/haarcascade_frontalface_alt.xml")
-    (options, args) = parser.parse_args()
-
-    cascade = cv.Load(options.cascade)
-
-    if len(args) != 1:
-        parser.print_help()
-        sys.exit(1)
-
-    input_name = args[0]
-    if input_name.isdigit():
-        capture = cv.CreateCameraCapture(int(input_name))
-    else:
-        capture = None
-
-    cv.NamedWindow("result", 1)
-
-    width = 320  # leave None for auto-detection
-    height = 240  # leave None for auto-detection
-
-    if width is None:
-        width = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH))
-    else:
-        cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_WIDTH, width)
-
-    if height is None:
-        height = int(cv.GetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT))
-    else:
-        cv.SetCaptureProperty(capture, cv.CV_CAP_PROP_FRAME_HEIGHT, height)
-
-    if capture:
-        frame_copy = None
-        while True:
-
-            frame = cv.QueryFrame(capture)
-            if not frame:
-                cv.WaitKey(0)
-                break
-            if not frame_copy:
-                frame_copy = cv.CreateImage((frame.width, frame.height),
-                                            cv.IPL_DEPTH_8U, frame.nChannels)
-
-            # frame_copy = cv.CreateImage((frame.width,frame.height),
-            #                                            cv.IPL_DEPTH_8U, frame.nChannels)
-
-            if frame.origin == cv.IPL_ORIGIN_TL:
-                cv.Copy(frame, frame_copy)
-            else:
-                cv.Flip(frame, frame_copy, 0)
-
-            detect_and_draw(frame_copy, cascade)
-
-            if cv.WaitKey(10) >= 0:
-                break
-    else:
-        image = cv.LoadImage(input_name, 1)
-        detect_and_draw(image, cascade)
-        cv.WaitKey(0)
-
-    cv.DestroyWindow("result")
+    print "captured % d "% (i)
