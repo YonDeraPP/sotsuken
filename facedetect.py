d9d7e9e7942133d5eeeeea9f37945c57bb6d3617
dera <deraderapyon@gmail.com>
2017-05-01 15:32:38 +0900

    inmi

--- facedetect.py
+++ facedetect.py
@@ -21,36 +21,21 @@ camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
 
 
 for i in xrange(5):
-    # 写真を撮る！
     camera.capture(stream, format='jpeg')
     data = np.fromstring(stream.getvalue(), dtype=np.uint8)
     image = cv2.imdecode(data, 1)
 
-    # エラーになるのでコメントアウト(ウィンドウに表示)
-    #        cv2.imshow('image',image)
-    #        cv2.waitKey(16)
-
-    # 解析しやすくするために灰色にする
     image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
-    # ライブラリに顔検出してもらう
     facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
     image_output = image
-    # 顔が１つ以上あったら
     if len(facerect) > 0:
-        # 顔検出した部分を、四角で囲む
         for rect in facerect:
             cv2.rectangle(image_output, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
         #
         print "found"
-    # エラーになるのでコメントアウト(ウィンドウに表示)
-    #        cv2.imshow('image_out',image_output)
-    #        cv2.waitKey(16)
-
-    # jpgファイルに保存
     cv2.imwrite(str(i) + '.jpg', image)
 
-    # 写真領域をクリア(コレをしないと、ずっと同じ写真になる)
     stream.seek(0)
 
     print "captured % d "% (i)
