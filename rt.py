797e5c57074fb857c348815d0957b3d2e55ee881
dera <deraderapyon@gmail.com>
2017-05-02 17:51:50 +0900

    kbtit

--- rt.py
+++ rt.py
@@ -4,6 +4,9 @@ import cv2
 
 import numpy as np
 
+cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_path)
+
 stream = io.BytesIO()
 
 CAMERA_WIDTH = 320
@@ -16,6 +19,16 @@ for i in xrange(120):
         camera.capture(stream, format='jpeg')
         data = np.fromstring(stream.getvalue(), dtype=np.uint8)
         image = cv2.imdecode(data, 1)
+
+        image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+        facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+
+        image_output = image
+        if len(facerect)>0:
+            for rect in facerect:
+                cv2.rectangle(image_output, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+                print "found"
+                
         cv2.imshow('image',image)
         cv2.waitKey(16)
         stream.seek(0)
