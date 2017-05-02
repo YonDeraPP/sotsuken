e442f05207dc0938714b4876bb9d62bca4b66e6c
dera <deraderapyon@gmail.com>
2017-05-02 18:34:51 +0900

    yj

--- rt.py
+++ rt.py
@@ -16,20 +16,24 @@ camera = picamera.PiCamera()
 camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
 color = (255, 255, 255)
 
-for i in xrange(120):
-        camera.capture(stream, format='jpeg')
-        data = np.fromstring(stream.getvalue(), dtype=np.uint8)
-        image = cv2.imdecode(data, 1)
+print "Type Ctrl+C to Stop"
 
-        image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
-        facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+if __name__ == "__main__":
+    while True:
+            camera.capture(stream, format='jpeg')
+            data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+            image = cv2.imdecode(data, 1)
 
-        image_output = image
-        if len(facerect)>0:
-            for rect in facerect:
-                cv2.rectangle(image_output, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
-                print "found"
+            image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+            facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
 
-        cv2.imshow('image',image)
-        cv2.waitKey(16)
-        stream.seek(0)
+            image_output = image
+            if len(facerect)>0:
+                for rect in facerect:
+                    cv2.rectangle(image_output, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+                    print "found"
+
+
+            cv2.imshow('image',image)
+            cv2.waitKey(16)
+            stream.seek(0)
