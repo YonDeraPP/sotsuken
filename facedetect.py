1030fe03ddb80c637eaa691ec8f0ba4a3bb00c41
dera <deraderapyon@gmail.com>
2017-05-02 15:36:16 +0900

    yj

--- facedetect.py
+++ facedetect.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 
 
-cascade_path = "/ usr / share / opencv / haarcascades / haarcascade_frontalface_alt.xml"
+cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
 cascade = cv2.CascadeClassifier(cascade_path)
 
 stream = io.BytesIO()
