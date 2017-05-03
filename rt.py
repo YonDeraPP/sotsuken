b956aa86fc034812b32889b0120236d40608fd3a
dera <deraderapyon@gmail.com>
2017-05-03 13:16:55 +0900

    yj

--- rt.py
+++ rt.py
@@ -9,19 +9,18 @@ import numpy as np
 cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
 cascade = cv2.CascadeClassifier(cascade_path)
 
-stream = io.BytesIO()
-
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
 
-camera = picamera.PiCamera()
-camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
 color = (255, 255, 255)
-camera.framerate = 24
-
-print "Type Ctrl+C to Stop"
 
 if __name__ == "__main__":
+    print "Type Ctrl+C to Stop"
+    with picamera.PiCamera() as camera:
+        camera.resolution(CAMERA_WIDTH,CAMERA_HEIGHT)
+        camera.framerate=24
+        stream = io.BytesIO
+
     while True:
             #picamera to opencv
             camera.capture(stream, format='jpeg',use_video_port=True)
