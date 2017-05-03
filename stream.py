03c8145f75d58d93a06f042766026f9a834bdca3
dera <deraderapyon@gmail.com>
2017-05-03 16:37:55 +0900

    eee

--- stream.py
+++ stream.py
@@ -6,6 +6,10 @@ from picamera import PiCamera
 import time
 import cv2
 
+cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_path)
+color = (255,255,255)
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -20,7 +24,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-
+    image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+    image_output = image
+    if len(facerect) > 0:
+        for rect in facerect:
+            cv2.rectangle(image_output, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+            print "found"
     # show the frame
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
