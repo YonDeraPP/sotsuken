4e4e3d9bd7474d9d9f86d0fda8b1df1b3db2d2cc
dera <deraderapyon@gmail.com>
2017-05-03 14:42:50 +0900

    eee

--- stream.py
+++ stream.py
@@ -4,6 +4,8 @@
 import time
 import picamera
 import io
+import  numpy as np
+import cv2
 
 camera = picamera.PiCamera()
 camera.resolution = (1024, 768)
@@ -12,6 +14,8 @@ stream = io.BytesIO()
 
 for foo in camera.capture_continuous(stream, "jpeg", use_video_port=True):
     stream.seek(0)
-    frame = stream.getvalue() #これで１フレーム取り出せる
+    frame = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    data = cv2.imdecode(frame, 1)
+    cv2.imshow('image',data)
     stream.seek(0)
     stream.truncate()
