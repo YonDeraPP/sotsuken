ca7cdad3f5925ab2007a3493ed3642c72388b1a7
dera <deraderapyon@gmail.com>
2017-05-10 15:18:08 +0900

    inmu

--- ctest.py
+++ ctest.py
@@ -5,19 +5,19 @@ import cv2
 
 import numpy as np
 
-def capture(mirror=True,size=None):
-    with picamera.PiCamera() as camera:
-        camera.resolution = (640, 480)
-        camera.framerate = 24
-        stream = io.BytesIO()
 
-        while True:
-            camera.capture(stream, format="jpeg", use_video_port=True)
-            frame = np.fromstring(stream.getvalue(), dtype=np.uint8)
-            stream.seek(0)
-            frame = cv2.imdecode(frame, 1)
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.framerate = 24
+    stream = io.BytesIO()
 
-            cv2.imshow('capture',frame)
-  
+    while True:
+        camera.capture(stream, format="jpeg", use_video_port=True)
+        frame = np.fromstring(stream.getvalue(), dtype=np.uint8)
+        stream.seek(0)
+        frame = cv2.imdecode(frame, 1)
 
-        cv2.destroyAllWindows()
+        cv2.imshow('capture',frame)
+
+
+    cv2.destroyAllWindows()
