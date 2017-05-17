aeec6c731e82aa173543b1f6ffb9a6e95d154596
dera <deraderapyon@gmail.com>
2017-05-17 15:01:30 +0900

    doe

---
+++ mailtest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import picamera
+import io
+import numpy as np
+import cv2
+
+stream = io.BytesIO
+
+cascade_path = "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml"
+cascade = cv2.CascadeClassifier(cascade_path)
+
+CAMERA_WIDTH = 320
+CAMERA_HEIGHT = 240
+
+color = (255,255,255)
+camera = picamera.PiCamera()
+camera.resolution = (CAMERA_WIDTH, CAMERA_HEIGHT)
+
+def main():
+    while True:
+        camera.capture(stream,format='jpeg')
+        data = np.fromstring(stream.getvalue(),dtype=np.uint8)
+        image = cv2.imread(data,1)
+
+        image_gray = cv2.cvtColor(image, cv2.cv.CV_BGR2GRAY)
+        facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
+
+        if len(facerect)>0:
+            print 'detected'
+
+
+if __name__ == '__main__':
+    main()
