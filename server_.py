7b3850baaad776f07501a863799f7437278b214f
dera <deraderapyon@gmail.com>
2017-05-30 19:48:21 +0900

    inmu

--- server_.py
+++ server_.py
@@ -11,6 +11,9 @@ import picamera
 CAMERA_WIDTH = 320
 CAMERA_HEIGHT = 240
 
+HOST = "192.168.10.46"
+PORT = 8000
+
 stream = io.BytesIO()
 
 camera = picamera.PiCamera()
@@ -18,8 +21,28 @@ camera.resolution = (CAMERA_WIDTH,CAMERA_HEIGHT)
 
 def capture():
     data = stream.getvalue()
-    return type(data)
+    return data
+
+class TCPHandler(SocketServer.BaseRequestHandler):
+    capture = ' '
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        self.request.send(capture())
+
+
 
 if __name__ == '__main__':
-    while True:
-        print capture()
+    SocketServer.TCPServer.allow_reuse_address = True
+    server = SocketServer.TCPServer((HOST, PORT), TCPHandler)
+    server.capture = capture
+
+    print "start server"
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.shutdown()
+    sys.exit()
+
