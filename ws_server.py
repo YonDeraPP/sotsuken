67f8cd3de237ebd042bc238e8b7d34db81c20aaa
YonDeraPP <deraderapyon@gmail.com>
2017-06-02 15:08:40 +0900

    6/2

--- ws_server.py
+++ ws_server.py
@@ -16,29 +16,28 @@ cl = []
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     # websocketオープン
     def open(self):
-        print "open"
+        print("open")
         if self not in cl:
             cl.append(self)
 
     # 処理
     def on_message(self, message):
-        print "on_message"
+        print("on_message")
         for client in cl:
-            print message[:4]
             data = np.fromstring(message.decode('base64'),dtype=np.uint8)
             img = cv2.imdecode(data,1)
             image_gray = cv2.cvtColor(img, cv2.cv.CV_BGR2GRAY)
             facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=1,minSize=(1,1))
             if len(facerect) > 0:
                 client.write_message("found!")
-                print "found"
+                print("found")
             else:
                 client.write_message("Not found!")
-                print "Not found"
+                print("Not found")
 
     # websockeクローズ
     def on_close(self):
-        print "close"
+        print("close")
         if self in cl:
             cl.remove(self)
 
