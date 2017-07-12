375d98d46ae24371a616a96a9550ddd5985cc56b
dera <deraderapyon@gmail.com>
2017-07-12 15:53:30 +0900

    7/12

--- flasktest.py
+++ flasktest.py
@@ -1,11 +1,34 @@
 from flask import Flask
 
+import requests
+import json
+import picamera
+import io
+import base64
+
+CAMERA_WIDTH = 320
+CAMERA_HEIGHT = 240
+stream = io.BytesIO()
+
+camera = picamera.PiCamera()
+camera.resolution = (CAMERA_WIDTH,CAMERA_HEIGHT)
 app = Flask(__name__)
 
+def Capture():
+    camera.capture('image.jpg')
+    data = open("image.jpg","rb")
+    return data
+
+
+url = 'http://192.168.10.63:8000/'
 
 @app.route('/')
 def index():
     print("hello")
+    data = Capture()
+    files = {'upload': data}
+    res = requests.post(url, files=files)
+    print(res)
     return 'hello'
 
 
