4e3fa229b9ee375d38c3d307859032c7e716a038
YonDeraPP <deraderapyon@gmail.com>
2017-08-24 13:42:19 +0900

    810

--- h_server.py
+++ h_server.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 from flask import Flask
 from flask import request
@@ -15,9 +15,7 @@ from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 
-
 app = Flask(__name__)
-
 
 
 class sendGmail:
@@ -34,9 +32,9 @@ class sendGmail:
 
         msg = self.create(to, sub, body, mime, attach_file)
         print('create ok')
-        self.send(host, port ,to , msg)
+        self.send(host, port, to, msg)
 
-    def create(self, to, sub, body , mime=None, attach_file=None):
+    def create(self, to, sub, body, mime=None, attach_file=None):
         msg = MIMEMultipart()
         body = MIMEText(body)
         msg['Subject'] = sub
@@ -45,7 +43,7 @@ class sendGmail:
         msg.attach(body)
 
         if mime != None and attach_file != None:
-            attachment = MIMEBase(mime['type'],mime['subtype'])
+            attachment = MIMEBase(mime['type'], mime['subtype'])
             file = open(attach_file['path'], 'rb')
             attachment.set_payload(file.read())
             file.close()
@@ -55,8 +53,8 @@ class sendGmail:
         print('create ok')
         return msg
 
-    def send(self,host, port, to, msg):
-        smtp = smtplib.SMTP_SSL(host,port)
+    def send(self, host, port, to, msg):
+        smtp = smtplib.SMTP_SSL(host, port)
         print('smtp ok')
         smtp.ehlo()
         print('ehlo ok')
@@ -67,20 +65,22 @@ class sendGmail:
         smtp.data(msg.as_string())
         smtp.quit()
 
+
 class bodyDetect:
     cascade_path = "/usr/local/Cellar/opencv/2.4.13/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
     Cascade = cv2.CascadeClassifier(cascade_path)
     color = (255, 0, 0)
+
     def __init__(self, img_src):
         self.src = cv2.imread(img_src, 1)
         self.result = self.src
         print('init')
-        #self.detect()
+        # self.detect()
 
     def rectangle(self):
         img_gray = cv2.cvtColor(self.src, cv2.COLOR_BGR2GRAY)
         print('image gray')
-        cv2.imwrite('./gray.jpg',img_gray)
+        cv2.imwrite('./gray.jpg', img_gray)
         faces = self.Cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=1, minSize=(100, 100))
         ans = faces
         print('yarimasunele')
@@ -89,17 +89,18 @@ class bodyDetect:
             for face in faces:
                 coordinates = tuple(face[0:2])
                 length = tuple(face[0:2] + face[2:4])
-                cv2.rectangle(self.result, coordinates, length, self.color, thickness = 3)
+                cv2.rectangle(self.result, coordinates, length, self.color, thickness=3)
 
         cv2.imwrite('./result.jpg', self.result)
 
         return len(ans)
 
-@app.route('/',methods=['POST'])
+
+@app.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
         f = request.files['upload']
-        f.save("./"+f.filename)
+        f.save("./" + f.filename)
 
         pic = bodyDetect('./image.jpg')
         print('save')
@@ -107,12 +108,11 @@ def index():
             to = 'fji810official@gmail.com'
             sub = 'Python'
             body = '114514'
-            mime = {'type':'image', 'subtype':'jpeg'}
-            attach_file={'name':'result.jpg', 'path':'./result.jpg'}
-            sendGmail(to, sub, body,mime,attach_file)
+            mime = {'type': 'image', 'subtype': 'jpeg'}
+            attach_file = {'name': 'result.jpg', 'path': './result.jpg'}
+            sendGmail(to, sub, body, mime, attach_file)
             print('send email')
             print('To {0}   Sub {1} Body {2}'.format(to, sub, body))
-                
 
 
 if __name__ == '__main__':
